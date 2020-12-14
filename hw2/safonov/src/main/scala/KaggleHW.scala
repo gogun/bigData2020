538a771a5b541fe9115e9df60d0ddac48b2e5830
mrsandman5 <mrsandman138@gmail.com>
2020-12-14 18:25:50 +0300

    hw1 added + fixes

---
+++ hw2/safonov/src/main/scala/KaggleHW.scala
@@ -0,0 +1,96 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.GBTClassifier
+import org.apache.spark.ml.feature.{HashingTF, IDF, RegexTokenizer, StopWordsRemover, StringIndexer}
+import org.apache.spark.mllib.feature.Stemmer
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, lit, when}
+import org.apache.spark.sql.types.DataTypes
+
+object KaggleHW {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("hw2")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("OFF")
+
+    val train = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("/home/mrsandman5/bigData2020/hw2/safonov/src/main/resources/train.csv")
+      .filter(col("id").isNotNull)
+      .filter(col("target").isNotNull)
+      .filter(col("text").isNotNull)
+      .select("id", "text", "target")
+      .withColumnRenamed("target", "label")
+
+    val tokenizer = new RegexTokenizer()
+      .setInputCol("text")
+      .setOutputCol("words")
+      .setPattern("[\\W]")
+
+    val stopWordsRemover = new StopWordsRemover()
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("removed")
+
+    val stemmer = new Stemmer()
+      .setInputCol(stopWordsRemover.getOutputCol)
+      .setOutputCol("stemmed")
+      .setLanguage("English")
+
+    val hashingTF = new HashingTF()
+      .setInputCol(stemmer.getOutputCol)
+      .setNumFeatures(5000)
+      .setOutputCol("rowFeatures")
+
+    val idf = new IDF()
+      .setInputCol(hashingTF.getOutputCol)
+      .setOutputCol("features")
+
+    val stringIndexer = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+
+    val gbt = new GBTClassifier()
+      .setLabelCol(stringIndexer.getOutputCol)
+      .setFeaturesCol(idf.getOutputCol)
+      .setPredictionCol("target")
+      .setMaxIter(30)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, stopWordsRemover, stemmer, hashingTF, idf, stringIndexer, gbt))
+
+    val model = pipeline.fit(train)
+
+    val test = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("/home/mrsandman5/bigData2020/hw2/safonov/src/main/resources/test.csv")
+      .filter(col("id").isNotNull)
+      .filter(col("text").isNotNull)
+      .select("id", "text")
+
+    val predictions = model.transform(test)
+    val result = predictions.select(col("id"), col("target").cast(DataTypes.IntegerType))
+
+    val sample = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("/home/mrsandman5/bigData2020/hw2/safonov/src/main/resources/sample_submission.csv")
+      .select("id")
+
+    result.join(sample, sample.col("id").equalTo(result.col("id")), "right")
+      .select(sample.col("id"), when(result.col("id").isNull, lit(0))
+        .otherwise(col("target"))
+        .as("target"))
+      .write.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .save("/home/mrsandman5/bigData2020/hw2/safonov/src/main/resources/my_result.csv")
+
+    model.write.overwrite
+      .save("/home/mrsandman5/bigData2020/hw2/safonov/model/")
+  }
+}
--- hw2/safonov/src/main/scala/StreamingHW.scala
+++ hw2/safonov/src/main/scala/StreamingHW.scala
@@ -1,91 +1,44 @@
-import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.ml.classification.GBTClassifier
-import org.apache.spark.ml.feature.{HashingTF, IDF, RegexTokenizer, StopWordsRemover, StringIndexer}
-import org.apache.spark.mllib.feature.Stemmer
+import org.apache.spark.ml.PipelineModel
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, lit, when}
-import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
 object StreamingHW {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .appName("hw2")
+      .appName("hw3")
       .config("spark.master", "local")
       .getOrCreate()
 
-    val train = spark.read.format("csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load("/home/mrsandman5/bigData2020/hw1/safonov/src/main/resources/train.csv")
-      .filter(col("id").isNotNull)
-      .filter(col("target").isNotNull)
-      .filter(col("text").isNotNull)
-      .select("id", "text", "target")
-      .withColumnRenamed("target", "label")
+    spark.sparkContext.setLogLevel("OFF")
 
-    val tokenizer = new RegexTokenizer()
-      .setInputCol("text")
-      .setOutputCol("words")
-      .setPattern("[\\W]")
+    val schema = new StructType()
+      .add("id", StringType, nullable = true)
+      .add("text", StringType, nullable = true)
 
-    val stopWordsRemover = new StopWordsRemover()
-      .setInputCol(tokenizer.getOutputCol)
-      .setOutputCol("removed")
+    val inputData = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 8065)
+      .load()
 
-    val stemmer = new Stemmer()
-      .setInputCol(stopWordsRemover.getOutputCol)
-      .setOutputCol("stemmed")
-      .setLanguage("English")
+    val inputJson =
+      inputData.withColumn("json", from_json(col("value"), schema))
+        .select("json.*")
+        .select(col("id"), col("text"))
 
-    val hashingTF = new HashingTF()
-      .setInputCol(stemmer.getOutputCol)
-      .setNumFeatures(5000)
-      .setOutputCol("rowFeatures")
-
-    val idf = new IDF()
-      .setInputCol(hashingTF.getOutputCol)
-      .setOutputCol("features")
-
-    val stringIndexer = new StringIndexer()
-      .setInputCol("label")
-      .setOutputCol("indexedLabel")
-
-    val gbt = new GBTClassifier()
-      .setLabelCol(stringIndexer.getOutputCol)
-      .setFeaturesCol(idf.getOutputCol)
-      .setPredictionCol("target")
-      .setMaxIter(30)
-
-    val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, stopWordsRemover, stemmer, hashingTF, idf, stringIndexer, gbt))
-
-    val model = pipeline.fit(train)
-
-    val test = spark.read.format("csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load("/home/mrsandman5/bigData2020/hw1/safonov/src/main/resources/test.csv")
-      .filter(col("id").isNotNull)
-      .filter(col("text").isNotNull)
-      .select("id", "text")
-
-    val predictions = model.transform(test)
-    val result = predictions.select(col("id"), col("target").cast(DataTypes.IntegerType))
-
-    val sample = spark.read.format("csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load("/home/mrsandman5/bigData2020/hw1/safonov/src/main/resources/sample_submission.csv")
-      .select("id")
-
-    result.join(sample, sample.col("id").equalTo(result.col("id")), "right")
-      .select(sample.col("id"), when(result.col("id").isNull, lit(0))
-        .otherwise(col("target"))
-        .as("target"))
-      .write.format("csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .save("/home/mrsandman5/bigData2020/hw1/safonov/src/main/resources/my_result.csv")
-
+    val model = PipelineModel.read.load("model/")
+    inputJson.printSchema()
+    model.transform(inputJson.select(col("id"), col("text")))
+      .select(col("id"), col("target").as("target").cast(IntegerType))
+        .repartition(1)
+          .writeStream
+            .outputMode("append")
+              .format("com.databricks.spark.csv")
+                .option("header", "true")
+                .option("path", "src/main/resources/path/")
+                .option("checkpointLocation", "checkpointLocation/")
+                  .start()
+                    .awaitTermination()
   }
 }
